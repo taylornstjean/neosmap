e88d4b9ebee70d882eb95b1427c51dcdd7755554
taylorstjean <taylorst@uw.edu>
2023-12-21 01:27:49 -0800

    improved Chrome browser support

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from neosmap.web_interface.app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=app.config["FLASK_RUN_PORT"])
 
 # ------------------------------ END OF FILE ------------------------------
