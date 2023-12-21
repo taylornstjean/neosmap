a461d18d41d80b319a4184de0399a106d7405374
Taylor St Jean <taylorst@uw.edu>
2023-12-20 22:12:36 -0800

    bug fixes/GUI updates round 2

---
+++ static/js/info.js
@@ -0,0 +1,28 @@
+"use strict";
+
+window.onload = function() {
+    initClock();
+    document.getElementById("default-open").click();
+};
+
+function openContent(evt, contentName) {
+
+    if (typeof document !== 'undefined') {
+
+        let i, tabcontent, tablinks;
+
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        evt.currentTarget.className += " active";
+        contentName.style.display = "flex";
+    }
+}
