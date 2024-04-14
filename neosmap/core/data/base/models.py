cc967ebf769c07f69d9a52899eda8c3de0cb2cbc
taylorstjean <taylorst@uw.edu>
2024-04-14 03:20:50 -0700

    gui fixes, refactoring

--- neosmap/core/data/base/models.py
+++ neosmap/core/data/base/models.py
@@ -1,17 +1,9 @@
 import numpy as np
-from config import MONITOR_TIME_INTERVAL, MPC_NEO_KEY_MAP, DATA_SUBDIRS
+from config import MONITOR_TIME_INTERVAL, MPC_NEO_KEY_MAP
 from neosmap.core.caching import APICache
-from neosmap.core.api import retrieve_data_mpc
 from neosmap.logger import logger
-from datetime import datetime as dt
-from dataclasses import dataclass, asdict
 import pandas as pd
-import os
 import json
-from threading import Lock
-
-
-thread_lock = Lock()
 
 
 ###########################################################################
@@ -19,79 +11,14 @@ thread_lock = Lock()
 
 class NEOMonitorBase:
 
-    def __init__(self, user):
-
-        self.time_interval = MONITOR_TIME_INTERVAL
-        self.save_time = 12 * 3600  # 12 hours
-        self._updates_path = os.path.join(DATA_SUBDIRS["monitor"], "updates.json")
-        self._updates = []
-        self._update_occurred = False
-        self._user_model = user
-
-    def _update(self, first_pull=False) -> None:
-
-        logger.debug(f"Running update for NEO Monitor data (first_pull={first_pull})")
-
-        def _retrieve_data():
-            logger.debug("Retrieving data from MPC API")
-            loaded_data = retrieve_data_mpc()
-            return loaded_data
-
-        data = _retrieve_data()
-
-        logger.debug("Successfully retrieved data from MPC API")
-
-        # Parsing and formatting data
-        data_table = []
-        for entry in data:
-            parsed_entry = self._entry_parser(entry)
-            if parsed_entry:
-                data_table.append(parsed_entry)
-
-        self._DF = pd.DataFrame(np.array(data_table), columns=MPC_NEO_KEY_MAP.values())
-
-        if not first_pull:
-            self._check_changes()
-
-        cache_ = APICache(name="monitor", cache_type="monitor", cache_time=50)
-        cache_.save(data_table)
-
-    @staticmethod
-    def _entry_parser(entry):
-        if not isinstance(entry, dict):
-            return None
-
-        parsed = []
-        keys = [key for key in MPC_NEO_KEY_MAP.keys() if key != "vis"]
-        for key in keys:
-            parsed.append(entry[key])
-
-        return parsed
-
-    def check_update(self) -> None:
-        with thread_lock:
-            logger.debug("Checking for updates to NEO Monitor data")
-
-            try:
-                _cache = APICache.get_instance(name="monitor")
-                update_required = not _cache.valid or not _cache.verify
-                first_pull = not _cache.verify
-                logger.debug("Found cached NEO Monitor data")
-
-            except ValueError:
-                update_required = True
-                first_pull = True
-
-            if update_required:
-                self._update(first_pull=first_pull)
-            else:
-                self._update_occurred = False
-                self.load_record()
-
-            if self._update_occurred:
-                logger.debug("Update occurred, activating ping")
-
-                self._user_model.activate_ping(self._app)
+    def load_record(self):
+        logger.debug("Loading NEO Monitor record")
+        try:
+            with open(self._updates_path, "r") as f:
+                self._updates = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            with open(self._updates_path, "w") as f:
+                json.dump([], f)
 
     def _load_last_df(self):
         cache_ = APICache.get_instance("monitor")
@@ -104,113 +31,16 @@ class NEOMonitorBase:
 
         return pd.DataFrame(np.array(data), columns=MPC_NEO_KEY_MAP.values())
 
-    def _check_changes(self) -> None:
+    @staticmethod
+    def _entry_parser(entry):
+        if not isinstance(entry, dict):
+            return None
 
-        logger.debug("Checking for changes to stored NEO Monitor data from new data")
+        parsed = []
+        keys = [key for key in MPC_NEO_KEY_MAP.keys() if key != "vis"]
+        for key in keys:
+            parsed.append(entry[key])
 
-        _DF_last = self._load_last_df()
-
-        try:
-            diff_df = pd.merge(
-                _DF_last.objectName, self._DF.objectName, indicator=True, how="outer"
-            ).loc[lambda x: x["_merge"] != 'both']
-        except pd.errors.MergeError:
-            diff_df = pd.DataFrame()
-
-        new_objects = diff_df[diff_df["_merge"] == "right_only"]["objectName"]
-        removed_objects = diff_df[diff_df["_merge"] == "left_only"]["objectName"]
-
-        _changes = new_objects.to_list() + removed_objects.to_list()
-
-        _DF_last_trim = _DF_last.loc[~_DF_last["objectName"].isin(_changes)]
-        _DF_trim = self._DF.loc[~self._DF["objectName"].isin(_changes)]
-
-        _nobs_merge_cols = ["objectName", "nObs"]
-
-        try:
-            diff_df_nobs = pd.merge(
-                _DF_last_trim[_nobs_merge_cols], _DF_trim[_nobs_merge_cols],
-                how="outer", suffixes=("_i", "_f"), on="objectName"
-            ).loc[lambda x: x["nObs_i"] != x["nObs_f"]]
-        except pd.errors.MergeError:
-            diff_df_nobs = pd.DataFrame()
-
-        alter_nobs_objects = diff_df_nobs
-
-        if any(df.shape[0] >= 1 for df in [new_objects, removed_objects, alter_nobs_objects]):
-            self._update_occurred = True
-            logger.debug("Changes to NEO Monitor data were detected")
-        else:
-            logger.debug("No changes to NEO Monitor data were detected")
-            self._update_occurred = False
-
-        current_time = dt.utcnow()
-        current_time_frmt = current_time.strftime("%b %d  %H:%M")
-
-        def _id(obj, act, time):
-            return "{}-{}-{}".format(obj, act, time)
-
-        self.load_record()
-
-        @dataclass
-        class Entry:
-            id: str
-            banner: str
-            action: str
-            objectName: str
-            time: float = current_time_frmt
-            attribute: str = "none"
-            nObs_i: str = "none"
-            nObs_f: str = "none"
-
-            def dict(self):
-                return {k: str(v) for k, v in asdict(self).items()}
-
-        for object_ in removed_objects.to_list():
-            entry_id = _id(object_, "remove", int(current_time.timestamp()))
-            entry = Entry(banner="Object Removed", action="object-removal", objectName=object_, id=entry_id)
-            self._updates.insert(0, entry.dict())
-
-        for object_ in new_objects.to_list():
-            entry_id = _id(object_, "add", int(current_time.timestamp()))
-            entry = Entry(banner="Object Added", action="object-addition", objectName=object_, id=entry_id)
-            self._updates.insert(0, entry.dict())
-
-        for update in alter_nobs_objects.to_dict(orient="index").values():
-            entry_id = _id(update["objectName"], "nobs", int(current_time.timestamp()))
-            entry = Entry(banner="Object Updated", action="object-alter", objectName=update["objectName"], id=entry_id,
-                          attribute="nobs", nObs_i=update["nObs_i"], nObs_f=update["nObs_f"])
-            self._updates.insert(0, entry.dict())
-
-        self._update_record()
-
-    def _update_record(self):
-        logger.debug("Updating NEO Monitor record")
-        self._clean_record()
-        with open(self._updates_path, "w") as f:
-            json.dump(self._updates, f)
-
-    def _clean_record(self):
-        logger.debug("Cleaning NEO Monitor record")
-        to_delete = []
-        for i, entry in enumerate(self._updates):
-            _id = entry["id"]
-            entry_time = float(_id.split("-")[-1])
-            current_time = dt.utcnow().timestamp()
-
-            if (current_time - entry_time) > self.save_time:
-                to_delete.append(i)
-
-        for i in sorted(to_delete, reverse=True):
-            del self._updates[i]
-
-    def load_record(self):
-        logger.debug("Loading NEO Monitor record")
-        try:
-            with open(self._updates_path, "r") as f:
-                self._updates = json.load(f)
-        except (FileNotFoundError, json.JSONDecodeError):
-            with open(self._updates_path, "w") as f:
-                json.dump([], f)
+        return parsed
 
 # ------------------------------ END OF FILE ------------------------------
--- neosmap/core/data/monitor.py
+++ neosmap/core/data/monitor.py
@@ -1,12 +1,15 @@
 import numpy as np
-from config import CACHE_USER_DIR
+from config import MONITOR_TIME_INTERVAL, MPC_NEO_KEY_MAP, DATA_SUBDIRS, CACHE_USER_DIR
 from flask_login import current_user
-from .base import NEOMonitorBase
-from datetime import datetime as dt
-import pandas as pd
+from neosmap.core.data.base import NEOMonitorBase
 import os
 import json
+from neosmap.core.caching import APICache
+from neosmap.core.api import retrieve_data_mpc
 from neosmap.logger import logger
+from datetime import datetime as dt
+from dataclasses import dataclass, asdict
+import pandas as pd
 
 
 ###########################################################################
@@ -15,10 +18,16 @@ from neosmap.logger import logger
 class NEOMonitor(NEOMonitorBase):
     """Stores an access pointer to monitoring data for each user. Data is routinely updated by a backend process."""
 
-    def __init__(self, *args, **kwargs):
-        super(NEOMonitor, self).__init__(*args, **kwargs)
+    def __init__(self, user, *args, **kwargs):
+        super(NEOMonitorBase, self).__init__(*args, **kwargs)
 
         self._ignore_id_path = os.path.join(CACHE_USER_DIR, "{}_ignore_ids.json".format(current_user.id))
+        self._user_model = user
+        self.save_time = 12 * 3600  # 12 hours
+        self._updates_path = os.path.join(DATA_SUBDIRS["monitor"], "updates.json")
+        self.time_interval = MONITOR_TIME_INTERVAL
+        self._update_occurred = False
+        self._updates = []
 
     def get_ignore_ids(self, timestamp: bool = False) -> list | dict:
         """Retrieve the list of update IDs that have been ignored.
@@ -120,10 +129,171 @@ class NEOMonitor(NEOMonitorBase):
 class NEOMonitorDaemon(NEOMonitorBase):
     """NEO Monitor daemon class for use by the backend daemon thread."""
 
-    def __init__(self, app, *args, **kwargs):
-        super(NEOMonitorDaemon, self).__init__(*args, **kwargs)
+    @dataclass
+    class Entry:
+        id: str
+        banner: str
+        action: str
+        objectName: str
+        time: str
+        attribute: str = "none"
+        nObs_i: str = "none"
+        nObs_f: str = "none"
 
+        def dict(self):
+            return {k: str(v) for k, v in asdict(self).items()}
+
+    def __init__(self, app, user, *args, **kwargs):
+        super(NEOMonitorBase, self).__init__(*args, **kwargs)
+
+        self._updates_path = os.path.join(DATA_SUBDIRS["monitor"], "updates.json")
+        self._user_model = user
+        self.save_time = 12 * 3600  # 12 hours
         self._app = app
+        self.time_interval = MONITOR_TIME_INTERVAL
+        self._update_occurred = False
+        self._updates = []
+
+    def _update(self, first_pull=False) -> None:
+
+        logger.debug(f"Running update for NEO Monitor data (first_pull={first_pull})")
+
+        def _retrieve_data():
+            logger.debug("Retrieving data from MPC API")
+            loaded_data = retrieve_data_mpc()
+            return loaded_data
+
+        data = _retrieve_data()
+
+        logger.debug("Successfully retrieved data from MPC API")
+
+        # Parsing and formatting data
+        data_table = []
+        for entry in data:
+            parsed_entry = self._entry_parser(entry)
+            if parsed_entry:
+                data_table.append(parsed_entry)
+
+        self._DF = pd.DataFrame(np.array(data_table), columns=MPC_NEO_KEY_MAP.values())
+
+        if not first_pull:
+            self._check_changes()
+
+        cache_ = APICache(name="monitor", cache_type="monitor", cache_time=50)
+        cache_.save(data_table)
+
+    def check_update(self) -> None:
+        logger.debug("Checking for updates to NEO Monitor data")
+
+        try:
+            _cache = APICache.get_instance(name="monitor")
+            update_required = not _cache.valid or not _cache.verify
+            first_pull = not _cache.verify
+            logger.debug("Found cached NEO Monitor data")
+
+        except ValueError:
+            update_required = True
+            first_pull = True
+
+        if update_required:
+            self._update(first_pull=first_pull)
+        else:
+            self._update_occurred = False
+            self.load_record()
+
+        if self._update_occurred:
+            logger.debug("Update occurred, activating ping")
+
+            self._user_model.activate_ping(self._app)
+
+    def _check_changes(self) -> None:
+
+        logger.debug("Checking for changes to stored NEO Monitor data from new data")
+
+        _DF_last = self._load_last_df()
+
+        try:
+            diff_df = pd.merge(
+                _DF_last.objectName, self._DF.objectName, indicator=True, how="outer"
+            ).loc[lambda x: x["_merge"] != 'both']
+        except pd.errors.MergeError:
+            diff_df = pd.DataFrame()
+
+        new_objects = diff_df[diff_df["_merge"] == "right_only"]["objectName"]
+        removed_objects = diff_df[diff_df["_merge"] == "left_only"]["objectName"]
+
+        _changes = new_objects.to_list() + removed_objects.to_list()
+
+        _DF_last_trim = _DF_last.loc[~_DF_last["objectName"].isin(_changes)]
+        _DF_trim = self._DF.loc[~self._DF["objectName"].isin(_changes)]
+
+        _nobs_merge_cols = ["objectName", "nObs"]
+
+        try:
+            diff_df_nobs = pd.merge(
+                _DF_last_trim[_nobs_merge_cols], _DF_trim[_nobs_merge_cols],
+                how="outer", suffixes=("_i", "_f"), on="objectName"
+            ).loc[lambda x: x["nObs_i"] != x["nObs_f"]]
+        except pd.errors.MergeError:
+            diff_df_nobs = pd.DataFrame()
+
+        alter_nobs_objects = diff_df_nobs
+
+        if any(df.shape[0] >= 1 for df in [new_objects, removed_objects, alter_nobs_objects]):
+            self._update_occurred = True
+            logger.debug("Changes to NEO Monitor data were detected")
+        else:
+            logger.debug("No changes to NEO Monitor data were detected")
+            self._update_occurred = False
+
+        current_time = dt.utcnow()
+        current_time_frmt = current_time.strftime("%b %d  %H:%M")
+
+        def _id(obj, act, time):
+            return "{}-{}-{}".format(obj, act, time)
+
+        self.load_record()
+
+        for object_ in removed_objects.to_list():
+            entry_id = _id(object_, "remove", int(current_time.timestamp()))
+            entry = self.Entry(banner="Object Removed", action="object-removal", objectName=object_, id=entry_id,
+                               time=current_time_frmt)
+            self._updates.insert(0, entry.dict())
+
+        for object_ in new_objects.to_list():
+            entry_id = _id(object_, "add", int(current_time.timestamp()))
+            entry = self.Entry(banner="Object Added", action="object-addition", objectName=object_, id=entry_id,
+                               time=current_time_frmt)
+            self._updates.insert(0, entry.dict())
+
+        for update in alter_nobs_objects.to_dict(orient="index").values():
+            entry_id = _id(update["objectName"], "nobs", int(current_time.timestamp()))
+            entry = self.Entry(banner="Object Updated", action="object-alter", objectName=update["objectName"],
+                               id=entry_id, attribute="nobs", nObs_i=update["nObs_i"], nObs_f=update["nObs_f"],
+                               time=current_time_frmt)
+            self._updates.insert(0, entry.dict())
+
+        self._update_record()
+
+    def _update_record(self):
+        logger.debug("Updating NEO Monitor record")
+        self._clean_record()
+        with open(self._updates_path, "w") as f:
+            json.dump(self._updates, f)
+
+    def _clean_record(self):
+        logger.debug("Cleaning NEO Monitor record")
+        to_delete = []
+        for i, entry in enumerate(self._updates):
+            _id = entry["id"]
+            entry_time = float(_id.split("-")[-1])
+            current_time = dt.utcnow().timestamp()
+
+            if (current_time - entry_time) > self.save_time:
+                to_delete.append(i)
+
+        for i in sorted(to_delete, reverse=True):
+            del self._updates[i]
 
 
 # ------------------------------ END OF FILE ------------------------------
